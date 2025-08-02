1093d454432b7e977fef4d186c9a9e745a7b69f8
amandeep tyagi <199378217+amandeeptyagi@users.noreply.github.com>
2025-08-02 12:08:33 +0530

    setup local database

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,9 +2,11 @@ import { DATABASE_URL, NODE_ENV } from "./env.js";
 import pkg from "pg";
 const { Pool } = pkg;
 
+const isProduction = NODE_ENV === "production";
+
 const pool = new Pool({
   connectionString: DATABASE_URL,
-  ssl: NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ...(isProduction && { ssl: { rejectUnauthorized: false } })
 });
 
 export default pool;
